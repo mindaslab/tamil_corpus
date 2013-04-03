08c26bbfc9c71db28e5662396e829e61804a9019
Karthikeyan <karthikeyan@mindaslab.(none)>
2013-04-03 21:12:21 +0530

    corpus started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TamilCorpus::Application.config.secret_token = '2591b57bdadc35b6bd1f289b43a7f138e1eed79fd09d3dbf5b448aa576979e60dc0f295454f8fa8f91e5e267ec31c961214dd88825a7fee12d5eb2f4619f3423'
