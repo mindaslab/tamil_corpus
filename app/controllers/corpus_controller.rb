104cd87fb06cb3afdcf840bce6f933fbae2bca38
Karthikeyan A K <mindaslab@gmail.com>
2013-04-13 11:42:36 +0530

    corpus controller - some pages added

--- app/controllers/corpus_controller.rb
+++ app/controllers/corpus_controller.rb
@@ -1,4 +1,26 @@
 class CorpusController < ApplicationController
+  ##
+  # A gendral intro into Sorkandu project, why its here?
   def index
+  end
+  
+  ##
+  # How one can contribute to this project
+  def contribute
+  end
+  
+  ##
+  # Help regarding contribution, how to use sorkandu
+  def help
+  end
+  
+  ##
+  # A search search section where people can search for words
+  def search
+  end
+  
+  ##
+  # How to contact Sorkandu team, who theyu are?
+  def contact
   end
 end
